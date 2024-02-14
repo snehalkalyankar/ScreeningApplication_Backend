7d9fdab0cfe83d00a1b2da1be1e66fcaf0707cf7
Samr0619 <samruddha.sawant0619@gmail.com>
2024-02-14 15:23:26 +0530

    Test COmmit

--- screening/src/main/java/com/resumeScreening/controller/AuthenticationController.java
+++ screening/src/main/java/com/resumeScreening/controller/AuthenticationController.java
@@ -78,7 +78,7 @@ public class AuthenticationController {
     @PostMapping("/SignUp")
     public ResponseEntity<?> signUp(@RequestBody SignUpBean bean) {
         try {
-        	System.out.println("Hello");
+        	System.out.println("Hello User ");
         } catch (Exception e) {
             // TODO: handle exception
             e.printStackTrace();
