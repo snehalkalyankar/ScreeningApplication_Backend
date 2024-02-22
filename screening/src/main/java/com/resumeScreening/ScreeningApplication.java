17e34de88cc638143ee0a92c51a7e2bf39d242f3
Samr0619 <samruddha.sawant0619@gmail.com>
2024-02-22 16:31:56 +0530

    Login API Changes

--- screening/src/main/java/com/resumeScreening/ScreeningApplication.java
+++ screening/src/main/java/com/resumeScreening/ScreeningApplication.java
@@ -2,7 +2,6 @@ package com.resumeScreening;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
